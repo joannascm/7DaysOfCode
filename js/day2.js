68e53e2d828fe3d439f043354ad1dbab754f8f49
joanna <joannasabrinacm@gmail.com>
2023-02-09 15:30:34 -0300

    Alterações CSS e day2

--- js/day2.js
+++ js/day2.js
@@ -3,11 +3,11 @@ function resultado() {
 
 const nome = document.getElementById("nome").value;
 const idade = document.getElementById("idade").value;
-const linguagem = document.getElementById("linguagem").value;
+const linguagem = document.getElementById("linguagemDeEstudo").value;
 const gostando = document.getElementById ("gostando").value;
 const resposta = document.getElementById("resposta");
 
-if (gostando == 1){
+if (gostando == 1) {
   resposta.innerHTML = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}, que bom que está gostando dessa linguagem! Continue estudando e você terá muito sucesso.`
 } else if (gostando == 2) {
   resposta.innerHTML = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}, que pena que não esta gostando, já tentou aprender outras linguagens?`
